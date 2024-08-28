03d9e5ddf8821b7405e4b1d02fc4b0a017c33034
Maximilian Klimko <82115185+Mamigako@users.noreply.github.com>
2024-08-28 11:47:34 +0000

    Precision loss compensated.

--- JavaProjects/src/PiDigits.java
+++ JavaProjects/src/PiDigits.java
@@ -3,39 +3,51 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class PiDigits {
     
+    // Calculate the amount of digits of pi that can be calculated within a given amount of seconds using binary search.
 
     public static void main(String[] args) {
         
-        long seconds = StdIn.readLong();
+        // Input secconds.
+        double seconds = StdIn.readDouble();
         
-        long hi = (long) (1.34*Math.pow(10, 6));
+        // Initiate high end value.
+        long hi = (long) (Math.pow(10, 15));
         
+        // Initiate low end value.
         long lo = 0;
         
-        long mid = lo + ((hi - lo)/2);
+        // Calculate midpoint.
+        long mid = (hi + lo)/2;
         
         double calc = 0;
 
         while (lo <= hi){
-                    
+            
+            // cast the midpoint into double to calculate amount of seconds using the midpoint as a guess for amount of digits.
             double mid_calc = (double) mid;
             calc = (mid_calc*Math.log10(mid_calc)) / Math.pow(10, 6);
             mid = (long) mid_calc;
  
+            // if the estimate is smaller than given seconds, assign midpoint+1 to low end.
             if ((calc < seconds)){
             lo = mid + 1;}
 
+            // If the estimate is larger than given seconds, assign midpoint-1 to high end.
             else if (calc > seconds){
             hi = mid -1;
             }
             
+            // If it´s equal(unlikely), print out midpoint as final answer.(Result)
             else {
             StdOut.print(mid);
             System.exit(0);}
 
-            mid = lo + (hi - lo)/2;
+            // find new midpoint.
+            mid = (hi + lo)/2;
 
         }
+
+        // Print amount of digits.(Result)
         StdOut.print(mid);
     }
 }
