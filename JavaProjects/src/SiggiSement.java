00480ed9a5da7771b565df865fb82ab1dd907b42
Maximilian Klimko <82115185+Mamigako@users.noreply.github.com>
2024-09-18 14:51:37 +0000

    added comments

--- JavaProjects/src/SiggiSement.java
+++ JavaProjects/src/SiggiSement.java
@@ -4,26 +4,35 @@ import java.util.Arrays;
 
 
 public class SiggiSement {
-    
+
+    //Finding out which 2 bags of cement can fill a hole of a certain size.
+    //each bag can only be chosen once.
 
     public static void main(String[] args) {
         
+        //Read in amount of bags and hole size.
         String[] bags_and_weight = StdIn.readLine().split(" ");
+        //If there is only one bag, exit.
         if (bags_and_weight[0] == "1"){StdOut.print("Neibb"); return;}
 
+        //If the hole size is below max int capacity use int.
         if (Long.parseLong(bags_and_weight[1]) < 2147483647) {
         
+        //Cast number of bags and hole size into int.
         int bags = Integer.parseInt(bags_and_weight[0]);
         int weight = Integer.parseInt(bags_and_weight[1]);
         int[] bag = new int[bags];
         int[] chosen_bags = new int[2];
 
+        //Read bags into int array.
         for (int i = 0; i < bags; i++){
             bag[i] = StdIn.readInt();
         }
 
+        //Sort the array.
         Arrays.sort(bag);
 
+        //Find bags that equal hole size using 2 pointers traversing the array from both sides and comparing sum of elements.
         int left = 0;
         int right = bags - 1;
         boolean found = false;
@@ -45,6 +54,7 @@ public class SiggiSement {
                 right--;
         }
            
+        // If bag not found, exit. Else print both bags.
         if (!found){
             StdOut.print("Neibb");} 
         else {
@@ -52,14 +62,13 @@ public class SiggiSement {
         }
 
 }
+
+    // Same as above, but using long.
     else { 
 
         int bags = Integer.parseInt(bags_and_weight[0]);
-
         long weight = Long.parseLong(bags_and_weight[1]);
-
         long[] bag = new long[(int)bags];
-
         long[] chosen_bags = new long[2];
 
     for (int i = 0; i < bags; i++){
@@ -91,10 +100,8 @@ public class SiggiSement {
     if (!found){
         StdOut.print("Neibb");} 
     else {
-    StdOut.print(chosen_bags[0] + " " + chosen_bags[1]);
+    StdOut.print(chosen_bags[0] + " " + chosen_bags[1]);}
     
-}
-    }
-
+        }
     }
 }
